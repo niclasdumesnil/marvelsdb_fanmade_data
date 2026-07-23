3c06b8380978d92187a00d30299c8b06ada42324
Jordan Weiler <jordan.weiler@emberex.com>
2026-07-22 22:58:49 -0700

    Add Fear No Evil pack and hero cards

--- add_octgnid.py
+++ add_octgnid.py
@@ -1,8 +1,8 @@
 import json
 import uuid
 
-runFile = 'hercules.json'
-pack_code = 'hercules'
+runFile = 'fear_no_evil.json'
+pack_code = 'fne'
 
 with open('./packs.json') as json_file:
     pack_data = json.load(json_file)
